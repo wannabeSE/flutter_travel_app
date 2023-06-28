6ecb339b6942090fda2aaf40fcf3d6636ae78a7c
unknown <samirk724@gmail.com>
2023-06-28 07:21:41 +0600

    responsiveness added to splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:travel_app/screens/main_screen.dart';
 import 'package:travel_app/screens/home.dart';
 import 'package:travel_app/screens/listing_screen.dart';
@@ -6,19 +7,48 @@ import 'package:travel_app/screens/profile_screen.dart';
 import 'package:travel_app/screens/splash_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/splashScreen',
-      routes: {
-        '/splashScreen' :(context) => const SplashScreen(),
-        '/main' :(context) => const MainScreen(),
-        '/home' :(context) => const HomeScreen(),
-        '/listing' :(context) => const ListingScreen(),
-        '/profile' : (context) => const ProfileScreen(),
-        
-      },
-    )
-  );
+  runApp(const MyApp());
 }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+@override
+  Widget build(BuildContext context){
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      //designSize: const Size(360, 690),
+      minTextAdapt: true,
+      builder: (context, child){
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: '/splashScreen',
+          routes: {
+            '/splashScreen' :(context) => const SplashScreen(),
+            '/main' :(context) => const MainScreen(),
+            '/home' :(context) => const HomeScreen(),
+            '/listing' :(context) => const ListingScreen(),
+            '/profile' : (context) => const ProfileScreen(),
+            
+          },
+        );
+      }
+    );
+  }
+}
+//     ScreenUtilInit(
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         initialRoute: '/splashScreen',
+//         routes: {
+//           '/splashScreen' :(context) => const SplashScreen(),
+//           '/main' :(context) => const MainScreen(),
+//           '/home' :(context) => const HomeScreen(),
+//           '/listing' :(context) => const ListingScreen(),
+//           '/profile' : (context) => const ProfileScreen(),
+          
+//         },
+//       ),
+//     )
+//   );
+// }
 
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,81 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:travel_app/constants/fonts_and_colors.dart';
 
 import '../shared/text_style.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
-
-  final TextStyle textStyle = const TextStyle(
-    fontFamily: sans,
-    fontSize: 32,
-    fontWeight: FontWeight.w600,
-  );
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        Column(
-          children: [
-            Image.asset(
-              'assets/icons/splash.png',
-              scale: 0.8,
+      body: Stack(
+        children: [
+          Container(
+            height: 812.h,
+            width: 375.w,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage('assets/icons/splash.png')
+              )
             ),
-          ],
-        ),
+          ),
+
         Padding(
-          padding: const EdgeInsets.all(24.0),
+          padding: EdgeInsets.all(24.sp),
           child: Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              height: 300,
-              width: 327,
+              height: 300.h,
+              width: 327.w,
               decoration: BoxDecoration(
                   color: dialogBoxColor.withOpacity(0.7),
-                  borderRadius: BorderRadius.circular(32)),
+                  borderRadius: BorderRadius.circular(32.sp)
+                ),
               child: Column(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(52, 24, 51, 16),
+                    padding: EdgeInsets.fromLTRB(52.sp, 24.sp, 51.sp, 16.sp),
                     child: SizedBox(
-                      width: 224,
+                      width: 224.w,
                       child: Text(
                         'Go explore\nOut there',
                         textAlign: TextAlign.center,
-                        style: textStyle,
+                        style: TextStyle(
+                          fontFamily: sans,
+                          fontSize: 32.sp,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ),
                   ),
-                  const Text(
+                  Text(
                     'Best solution for your tour\nvacation camping',
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                        fontFamily: sans,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400),
+                      fontFamily: sans,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w400
+                    ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(24),
+                    padding: EdgeInsets.all(24.sp),
                     child: SizedBox(
-                        height: 70,
-                        width: 279,
+                        height: 70.h,
+                        width: 279.w,
                         child: ElevatedButton(
                           onPressed: () {
                             Navigator.pushNamed(context, '/main');
                           },
                           style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(35))),
-                          child: const SemiBoldText(text: 'Get Started', fontSize: 20),
-                        )),
-                  )
-                ],
+                            backgroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(35.sp)
+                            )
+                          ),
+                          child: SemiBoldText(
+                            text: 'Get Started', 
+                            fontSize: 20.sp
+                          ),
+                        )
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        )
-      ]),
+          )
+        ]
+      ),
     );
   }
 }
